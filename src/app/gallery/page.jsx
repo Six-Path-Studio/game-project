6ebc3f1343190527e0bea9532b9d7eac510714ad
alabo-excel <iamalaboexcel@gmail.com>
2024-03-31 04:50:48 +0100

    gallary update

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -32,8 +32,8 @@ const gallery = () => {
         <div className='border-2 border-[#CECECE] mx-auto rotate-[2.17deg] w-52 text-center p-3 rounded-md text-xl'>
           Our Gallery
         </div>
-        <div className='grid-cols-3 gap-3	grid lg:px-32 px-4 py-12'>
-          {images.map((single, index) => <img key={index} className='h-full my-3 object-cover' src={'/images/gallery/item-' + index + '.png'} />)}
+        <div className='lg:grid-cols-3 gap-3	grid lg:px-32 px-4 py-12'>
+          {images.map((single, index) => <img key={index} className='h-full my-4 object-cover' src={'/images/gallery/item-' + index + '.png'} />)}
         </div>
       </section>
       <CTA />
